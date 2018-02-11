329f93f2593a47fe5b366709f92c8ef53e0837d6
Your Name <aleksey.kolyasa.dev@gmail.com>
2018-02-11 23:04:05 +0300

    -- back errors msg -en/ru -- stylize -auth-panel

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,11 +6,11 @@ const customError = require('../utils/errors');
 exports.register = async (req, res, next) => {
 	const { login, password, confirmPassword, email, name } = req.body;
 
-	if (password !== confirmPassword) return next(customError(`Passwords validation failed!`, 403));
+	if (password !== confirmPassword) return next(customError(`Passwords validation failed! \ Пароли не совпадают!`, 403));
 
 	try {
 		const isExists = await User.findOne({ login });
-		if (isExists) return next(customError(`Such login already exists, try another one`, 403));
+		if (isExists) return next(customError(`Such login already exists, try another one \ Такой логин уже занят!`, 403));
 
 		const _newUser = new User({
 			login,
@@ -34,10 +34,10 @@ exports.login = async (req, res, next) => {
 
 	try {
 		const _user = await User.findOne({ login });
-		if (!_user) return next(customError(`User Not Found!`, 404));
+		if (!_user) return next(customError(`User Not Found! \ Пользователь не найден!`, 404));
 
 		const isMatch = await _user.comparePassword(password);
-		if (!isMatch) return next(customError(`Password incorrect!`, 403));
+		if (!isMatch) return next(customError(`Password incorrect! \ Неверный пароль!`, 403));
 
 		const onlineStatusUpdate = await _user.update({ $set: { isLogged: true, lastOnline: moment.now() } });
 		res.json(onlineStatusUpdate);
