1b53fe0e510e76303556b62468730b17be78687b
Your Name <aleksey.kolyasa.dev@gmail.com>
2018-02-11 16:27:06 +0300

    -- login --imp

--- public/src/app/auth/login/login.component.spec.ts
+++ public/src/app/auth/login/login.component.spec.ts
@@ -1,25 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LoginComponent } from './login.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {LoginComponent} from './login.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {LanguageService} from '../../@services/language.service';
+import {SharedModule} from '../../@shared/shared.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FormsModule,
+                ReactiveFormsModule,
+                SharedModule,
+                BrowserAnimationsModule,
+                TranslateModule.forChild(),
+            ],
+            declarations: [LoginComponent],
+            providers: [
+                LanguageService,
+            ],
+            schemas: [NO_ERRORS_SCHEMA],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- public/src/app/auth/login/login.component.ts
+++ public/src/app/auth/login/login.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import {PWD_PATTERN, TEXT_PATTERN} from '../../@constants/patterns/pattern.constants';
+import {LanguageService} from '../../@services/language.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    animations: [
+        trigger('fadeInOnInit', [
+            transition('* => fadeIn', [
+                style({ opacity: 0 }),
+                animate(500, style({ opacity: 1}))
+            ]),
+            transition('* => fadeOut', [
+                animate(400, style({ opacity: 0 }))
+            ])
+        ])
+    ]
 })
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    anime = '';
 
-  constructor() { }
+    constructor(private fb: FormBuilder,
+                private languageService: LanguageService) {
+        this.createForm();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.languageService.initLanguage();
+        this.fadeIn();
+    }
 
+    createForm() {
+        this.loginForm = this.fb.group(
+            {
+                login: [
+                    '',
+                    Validators.compose([
+                        Validators.required,
+                        Validators.minLength(2),
+                        Validators.pattern(TEXT_PATTERN)
+                    ]),
+                ],
+                password: [
+                    '',
+                    Validators.compose([
+                        Validators.required,
+                        Validators.minLength(6),
+                        Validators.pattern(PWD_PATTERN)
+                    ]),
+                ],
+            },
+        );
+    }
+
+    fadeIn() {
+        this.anime = 'fadeIn';
+    }
+    fadeOut() {
+        this.anime = 'fadeOut';
+    }
 }
