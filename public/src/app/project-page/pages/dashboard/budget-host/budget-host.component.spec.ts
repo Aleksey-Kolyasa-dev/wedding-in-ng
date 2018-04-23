2163698d30ec5bfecc92ff8b08d916c0cdb0f612
Your Name <aleksey.kolyasa.dev@gmail.com>
2018-04-23 05:15:33 +0300

    -F: budget table footer --inPro

--- public/src/app/project-page/pages/dashboard/budget-host/budget-host.component.spec.ts
+++ public/src/app/project-page/pages/dashboard/budget-host/budget-host.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {BudgetHostComponent} from './budget-host.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {TranslateModule} from '@ngx-translate/core';
 
 describe('BudgetHostComponent', () => {
     let component: BudgetHostComponent;
@@ -10,7 +11,9 @@ describe('BudgetHostComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [BudgetHostComponent],
-            imports: [],
+            imports: [
+                TranslateModule.forRoot(),
+            ],
             schemas: [NO_ERRORS_SCHEMA],
         })
             .compileComponents();
